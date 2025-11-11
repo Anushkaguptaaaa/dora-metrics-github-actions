4fc13e944fadec79f7108db1b43237e7e2948279
Anushka Gupta <aanushkaguptaa4@gmail.com>
2025-11-11 15:48:16 +0530

    minor text change

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 def main():
-    print("Hello DevOps - This is a sample CI pipeline test")
+    print("Hello DevOps")
     return "Hello DevOps - This is a sample CI pipeline test"
 
 if __name__ == "__main__":
